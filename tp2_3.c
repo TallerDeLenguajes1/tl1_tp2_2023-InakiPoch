f24687d949d54c147cf3339787b24851e76240ad
Inaki <inakipoch106@gmail.com>
2023-03-23 18:57:36 -0300

    Created tp2_3.c

--- tp2_3.c
+++ tp2_3.c
@@ -5,12 +5,13 @@
 #define N 5
 
 int main() {
-    int matrix[N][M];
+    double matrix[N][M];
     srand(time(NULL));
     for(int i = 0; i < N; i++) {
+        printf("\n");
         for(int j = 0; j < M; j++) {
-            matrix[i][j] = rand()%100 + 1;
-            printf("%lf\n",matrix[i][j]);
+            *(*(matrix + i) + j) = rand()%100 + 1;
+            printf("%lf    ",*(*(matrix + i) + j));
         }
     }
     return 0;
